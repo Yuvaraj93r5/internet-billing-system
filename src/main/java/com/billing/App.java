f2e1b54aa0e72708b5e817a7507ebc1e72b576df
Yuvaraj <yuvaraj.g2024@vitstudent.ac.in>
2026-09-03 19:19:34 +0530

    Fix billing application

--- src/main/java/com/billing/App.java
+++ src/main/java/com/billing/App.java
@@ -39,7 +39,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.util.System.in);
+        Scanner scanner = new Scanner(System.out.System.in);
         String choice;
 
         System.out.println("=== Internet Data Usage Billing System ===");
